0c4689d45af4d76a50b3b7f74137739f7213cf9f
Nikolay Balakin <n@balakin.me>
2018-09-30 19:49:51 +0300

    More complex stack memory management

--- StackMemoryManagement/Program.cs
+++ StackMemoryManagement/Program.cs
@@ -9,13 +9,16 @@ namespace StackMemoryManagement
         {
             SimpleMethod();
             ComplexMethod();
+
+            Console.WriteLine("Waiting 10 minutes...");
             Thread.Sleep(TimeSpan.FromMinutes(10));
+            Console.WriteLine("Done");
         }
 
         static void SimpleMethod()
         {
             var modem = new SimpleModem();
-            var phoneNumber = new PhoneNumber();
+            var phoneNumber = GetPhoneNumber();
             modem.Call(phoneNumber);
             modem.SendMessage(phoneNumber);
         }
@@ -23,28 +26,43 @@ namespace StackMemoryManagement
         static void ComplexMethod()
         {
             var modem = new ComplexModem();
-            var phoneNumber = new PhoneNumber();
+            var phoneNumber = GetPhoneNumber();
             var called = modem.Call(phoneNumber);
-            var messageSent=modem.SendMessage(phoneNumber);
+            var messageSent = modem.SendMessage(phoneNumber);
             if (!called || !messageSent)
             {
                 Console.WriteLine("Something went wrong");
             }
         }
+
+        static PhoneNumber GetPhoneNumber()
+        {
+            Console.Write("Enter the phone number: ");
+            var number = Console.ReadLine();
+            return new PhoneNumber(number);
+        }
     }
 
     public struct PhoneNumber
     {
+        public string Number { get; set; }
+
+        public PhoneNumber(string number)
+        {
+            Number = number;
+        }
     }
 
     public class SimpleModem
     {
         public void Call(PhoneNumber number)
         {
+            Console.WriteLine("Calling " + number.Number);
         }
 
         public void SendMessage(PhoneNumber number)
         {
+            Console.WriteLine("Sending message to " + number.Number);
         }
     }
 
@@ -52,12 +70,14 @@ namespace StackMemoryManagement
     {
         public bool Call(PhoneNumber number)
         {
-            return true;
+            Console.WriteLine("Calling " + number.Number);
+            return Console.ReadKey(true).Key == ConsoleKey.Enter;
         }
 
         public bool SendMessage(PhoneNumber number)
         {
-            return true;
+            Console.WriteLine("Sending message to " + number.Number);
+            return Console.ReadKey(true).Key == ConsoleKey.Enter;
         }
     }
 }
